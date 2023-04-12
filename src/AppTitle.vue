13d2fdf3c57d2eb7a4136af7799cc1f4d586e59b
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-04-12 12:48:34 +0200

    feat: :sparkles: Adds: basic construction (incomplete) of files composition

---
+++ src/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+    name: "AppTitle",
+    data() {
+        return {
+            title: "Ciao, sono il titolo principale"
+        }
+    }
+  }
+</script>
+
+<template>
+   
+</template>
+
+<style>
+</style>
